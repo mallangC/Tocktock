800eaa2af2d7f6281aedd3d9cfea8b6f80980457
Mallangc <cky9001@gmail.com>
2025-07-09 19:13:51 +0900

    init

---
+++ src/main/java/com/mllg/tocktock/TocktockApplication.java
@@ -0,0 +1,16 @@
+package com.mllg.tocktock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class TocktockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TocktockApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mllg/tocktock/type/MemberType.java
@@ -0,0 +1,5 @@
+package com.mllg.tocktock.type;
+
+public enum MemberType {
+  ADMIN, USER
+}
