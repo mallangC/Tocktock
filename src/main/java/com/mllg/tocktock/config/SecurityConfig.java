63b3690328ecbea111ec2953a946b0473ea575d6
Mallangc <cky9001@gmail.com>
2025-07-18 09:47:02 +0900

    main / SecurityConfig 수정

--- src/main/java/com/mllg/tocktock/config/SecurityConfig.java
+++ src/main/java/com/mllg/tocktock/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+    configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://tock-tock.com", "https://www.tock-tock.com"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
