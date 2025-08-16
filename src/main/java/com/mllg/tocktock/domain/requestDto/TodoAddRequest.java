15f649b6306fe331722432be52a76275dfca369e
Mallangc <cky9001@gmail.com>
2025-08-16 15:27:28 +0900

    dev / Todo 추가, 수정 내용 최대 글자수 변경 - 최대 60에서 80자로 변경

--- src/main/java/com/mllg/tocktock/domain/requestDto/TodoAddRequest.java
+++ src/main/java/com/mllg/tocktock/domain/requestDto/TodoAddRequest.java
@@ -13,6 +13,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TodoAddRequest {
   @NotBlank(message = "내용을 입력해주세요")
-  @Size(min = 1, max = 60, message = "내용은 1자 이상 60자 이하여야 합니다.")
+  @Size(min = 1, max = 80, message = "내용은 1자 이상 80자 이하여야 합니다.")
   private String content;
 }
--- src/main/java/com/mllg/tocktock/domain/requestDto/TodoUpdateContentRequest.java
+++ src/main/java/com/mllg/tocktock/domain/requestDto/TodoUpdateContentRequest.java
@@ -16,6 +16,6 @@ public class TodoUpdateContentRequest {
     @NotNull
     private Long id;
     @NotBlank(message = "내용을 입력해주세요")
-    @Size(min = 1, max = 40, message = "내용은 1자 이상 40자 이하여야 합니다.")
+    @Size(min = 1, max = 80, message = "내용은 1자 이상 80자 이하여야 합니다.")
     private String content;
 }
